eb98cb999f7deb39f7dbd28055040f5aec6fd1b4
GarrethSalang <149616852+GarrethSalang@users.noreply.github.com>
2023-12-10 19:10:51 -0600

    Update Songs.java

--- FXProjects/src/application/Songs.java
+++ FXProjects/src/application/Songs.java
@@ -3,32 +3,60 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 
-// Song class represents an individual song
-class Song {
+import java.util.ArrayList;
+import java.util.List;
+
+//The Song class will have ID for a single song
+class Song 
+{
+    //Declaring privage variables
     private String title;
     private String artist;
+    private String genre;
+    private int duration;
 
-    public Song(String title, String artist) {
+    public Song(String title, String artist, String genre, int duration) 
+    {
         this.title = title;
         this.artist = artist;
+        this.genre = genre;
+        this.duration = duration;
     }
 
-    public String getTitle() {
+    //Gets the title of a song
+    public String getTitle() 
+    {
         return title;
     }
 
-    public String getArtist() {
+    //Returns the name of an artist
+    public String getArtist() 
+    {
         return artist;
     }
 
-    @Override
-    public java.lang.String toString() {
-        return "Song: " + title + " by " + artist;
+    //Returns the genre of a song
+    public String getGenre() 
+    {
+        return genre;
     }
 
-    // Override equals and hashCode to compare songs based on title and artist
+    //Returns the duration of a song
+    public int getDuration() 
+    {
+        return duration;
+    }
+
+    //Returns all the strings for the songID
+    public String toString()
+    {
+        return "Song: " + title + " by " + artist + " (" + genre + ", " + duration + "s)";
+    }
+
+    //Equals and hashCode will determine if a song is already in the playlist
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(Object obj) 
+    {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         Song song = (Song) obj;
@@ -42,75 +70,166 @@ class Song {
 }
 
 // Playlist class represents a collection of songs
-class Playlist {
+class Playlist 
+{
     private String name;
     private List<Song> songs;
+    private String createdBy;
 
-    public Playlist(String name) {
+    public Playlist(String name, String createdBy) 
+    {
         this.name = name;
+        this.createdBy = createdBy;
         songs = new ArrayList<>();
     }
 
-    public String getName() {
+    //This will get the name of the playlist
+    public String getName() 
+    {
         return name;
     }
 
-    public void addSong(Song song) {
-        if (songs.contains(song)) {
+    //Returns the name of the creator of the playlist
+    public String getCreatedBy() 
+    {
+        return createdBy;
+    }
+
+    //This will add a song to the playlist
+    public void addSong(Song song) 
+    {
+    	//This will throw an exception if a song is already in the playlist
+        if (songs.contains(song)) 
+        {
             throw new IllegalArgumentException("Song already exists in the playlist");
         }
         songs.add(song);
     }
 
-    public void removeSong(Song song) {
+    //This will remove a song from the playlist
+    public void removeSong(Song song) 
+    {
         songs.remove(song);
     }
 
-    public List<Song> getSongs() {
+    //This will return the list of songs
+    public List<Song> getSongs() 
+    {
         return songs;
     }
 }
 
 // SongPlayer class simulates playing songs from a playlist
-class SongPlayer {
-    public void playSong(Song song) {
+class SongPlayer
+{
+    private List<Song> playlist;
+    private int currentSongIndex;
+    private boolean isPlaying;
+
+    public SongPlayer() 
+    {
+        this.playlist = new ArrayList<>(); // Using ArrayList to allow modifications
+        this.currentSongIndex = 0;
+    }
+
+    //This will play the song
+    public void play(Song song)
+    {
+        isPlaying = true;
         System.out.println("Now playing: " + song);
     }
 
-    public void playPlaylist(Playlist playlist) {
-        List<Song> songs = playlist.getSongs();
-        System.out.println("Playlist: " + playlist.getName());
-        for (Song song : songs) {
-            playSong(song);
+    //This will pause the song
+    public void pause() 
+    {
+        //if else statement if a song is playing or not
+        if (isPlaying) 
+        {
+            isPlaying = false;
+            System.out.println("Paused: " + playlist.get(currentSongIndex));
+        }
+        //
+        else
+        {
+            System.out.println("Already paused");
         }
     }
+
+    //This will skip the song
+    public void skip() 
+    {
+        if (!playlist.isEmpty()) 
+        {
+            //This will see if a song is currently playing and then skip it
+            if (isPlaying) 
+            {
+                System.out.println("Skipping: " + playlist.get(currentSongIndex));
+                currentSongIndex = (currentSongIndex + 1) % playlist.size();
+                play(playlist.get(currentSongIndex));
+            } 
+            //This will print out if there is no song currently playing
+            else 
+            {
+                System.out.println("Not currently playing. Cannot skip.");
+            }
+        } 
+        //This will print out the if the playlist is empty
+        else
+        {
+            System.out.println("Playlist is empty. Cannot skip.");
+        }
+    }
+
+    //This will stop the current song
+    public void stop() 
+    {
+        if (isPlaying)
+        {
+            isPlaying = false;
+            System.out.println("Stopped: " + playlist.get(currentSongIndex));
+        } 
+        else
+        {
+            System.out.println("Already stopped");
+        }
+    }
+
+    public void playPlaylist(Playlist playlist) 
+    {
+        stop();
+
+        this.playlist = playlist.getSongs();
+        currentSongIndex = 0;
+
+        if (!this.playlist.isEmpty()) 
+        {
+            play(this.playlist.get(currentSongIndex));
+        } 
+        else 
+        {
+            System.out.println("Playlist is empty. Cannot play.");
+        }
+    }
+
+
 }
 
-// Example usage
-public class Songs {
-    public static void main(String[] args) {
-        // Create songs
-        Song song1 = new Song("Song 1", "Artist 1");
-        Song song2 = new Song("Song 2", "Artist 2");
-        Song song3 = new Song("Song 3", "Artist 3");
+//main class
+public class Songs
+{
+    public static void main(String[] args) 
+    {
+        //This will create the songs
+        Song song1 = new Song("Song 1", "Jaden", "Rock", 180);
+        Song song2 = new Song("Song 2", "Garreth", "City Pop", 210);
+        Song song3 = new Song("Song 3", "Claude", "Jazz", 150);
 
-        // Create a playlist with a name
-        Playlist playlist = new Playlist("My Playlist");
+        //This will create a playlist with a name and createdBy information
+        Playlist playlist = new Playlist("THE Playlist", "Music Group");
 
-        // Add songs to the playlist
-        playlist.addSong(song1);
+        //This will add songs to the playlist
         playlist.addSong(song2);
+        playlist.addSong(song1);
         playlist.addSong(song3);
-
-        // Try to add a duplicate song (comment out this line to avoid the exception)
-        try {
-            playlist.addSong(new Song("Song 1", "Artist 1"));
-        } catch (IllegalArgumentException e) {
-            System.out.println("Exception: " + e.getMessage());
-        }
-
-        // Create a SongPlayer and play the playlist
-        SongPlayer songPlayer = new SongPlayer();
-        songPlayer.playPlaylist(playlist);
     }
 }
